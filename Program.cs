28e1e2c8ce0759452ddf7a00dd24c914f8413188
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-18 21:47:09 -0300

    resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            string texto;
+
+
+            Console.Write("Usuário, o que você pensa sobre mim? ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            texto = Console.ReadLine();
+            Console.ResetColor();
+
+
+            Console.WriteLine("- E sabe o que eu penso de você?");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write($"{texto.Remove(10)}");
+            Console.ResetColor();
+            Console.Write("... NÃO, PERA.\nTô nervoso");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Pressione uma tecla para finalizar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
